2bfb81b18edb67ed021efa2d8ab78b9d792305d4
SidorkinAlex <palach.orl@mail.ru>
2021-03-14 15:47:26 +0300

    Add module: List Modules for mobile

---
+++ custom/Extension/application/Ext/Include/ServerPackageAndroidAppSuiteCRM.php
@@ -0,0 +1,9 @@
+<?php 
+ //WARNING: The contents of this file are auto-generated
+$beanList['MBL_MODULE_PUBLIC_LIST'] = 'MBL_MODULE_PUBLIC_LIST';
+$beanFiles['MBL_MODULE_PUBLIC_LIST'] = 'modules/MBL_MODULE_PUBLIC_LIST/MBL_MODULE_PUBLIC_LIST.php';
+$modules_exempt_from_availability_check['MBL_MODULE_PUBLIC_LIST'] = 'MBL_MODULE_PUBLIC_LIST';
+$report_include_modules['MBL_MODULE_PUBLIC_LIST'] = 'MBL_MODULE_PUBLIC_LIST';
+$modInvisList[] = 'MBL_MODULE_PUBLIC_LIST';
+
+?>
---
+++ modules/MBL_MODULE_PUBLIC_LIST/language/ru_ru.lang.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2018 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+$mod_strings = array (
+  'LBL_ASSIGNED_TO_ID' => 'Ответственный(ая)',
+  'LBL_ASSIGNED_TO_NAME' => 'Ответственный(ая)',
+  'LBL_SECURITYGROUPS' => 'Группы пользователей',
+  'LBL_SECURITYGROUPS_SUBPANEL_TITLE' => 'Группы пользователей',
+  'LBL_ID' => 'ID',
+  'LBL_DATE_ENTERED' => 'Дата создания',
+  'LBL_DATE_MODIFIED' => 'Дата изменения',
+  'LBL_MODIFIED' => 'Изменено',
+  'LBL_MODIFIED_NAME' => 'Изменено',
+  'LBL_CREATED' => 'Создано',
+  'LBL_DESCRIPTION' => 'Описание',
+  'LBL_DELETED' => 'Удалено',
+  'LBL_NAME' => 'Название',
+  'LBL_CREATED_USER' => 'Создано',
+  'LBL_MODIFIED_USER' => 'Изменено',
+  'LBL_LIST_NAME' => 'Название',
+  'LBL_EDIT_BUTTON' => 'Править',
+  'LBL_REMOVE' => 'Удалить',
+  'LBL_ASCENDING' => 'Восходящая',
+  'LBL_DESCENDING' => 'Нисходящая',
+  'LBL_OPT_IN' => 'Подтвердить подписку',
+  'LBL_OPT_IN_PENDING_EMAIL_NOT_SENT' => 'Ожидание подтверждения подписки, письмо с запросом на подтверждение НЕ отправлено.',
+  'LBL_OPT_IN_PENDING_EMAIL_SENT' => 'Ожидание подтверждения подписки, отправлено письмо с запросом на подтверждение.',
+  'LBL_OPT_IN_CONFIRMED' => 'Двойное подтверждение подписки.',
+  'LBL_LIST_FORM_TITLE' => 'Modules for phone Список',
+  'LBL_MODULE_NAME' => 'Modules for phone',
+  'LBL_MODULE_TITLE' => 'Modules for phone',
+  'LBL_HOMEPAGE_TITLE' => 'Мой Modules for phone',
+  'LNK_NEW_RECORD' => 'Создать Modules for phone',
+  'LNK_LIST' => 'View Modules for phone',
+  'LNK_IMPORT_MBL_MODULE_PUBLIC_LIST' => 'Import Modules for phone',
+  'LBL_SEARCH_FORM_TITLE' => 'Найти Modules for phone',
+  'LBL_HISTORY_SUBPANEL_TITLE' => 'Просмотр истории',
+  'LBL_ACTIVITIES_SUBPANEL_TITLE' => 'Мероприятия',
+  'LBL_MBL_MODULE_PUBLIC_LIST_SUBPANEL_TITLE' => 'Modules for phone',
+  'LBL_NEW_FORM_TITLE' => 'Новый Modules for phone',
+  'Module name' => 'module name',
+);
